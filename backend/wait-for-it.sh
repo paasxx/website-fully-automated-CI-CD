eac3d552bbaf0e7165911cc4091561a276e62840
Pedro <paasxx@gmail.com>
2024-06-13 20:33:05 -0300

    Teste

--- backend/wait-for-it.sh
+++ backend/wait-for-it.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 # Use this script to test if a given TCP host/port are available
 
-TIMEOUT=15
+#!/usr/bin/env bash
+
+TIMEOUT=30
 QUIET=0
 HOST="$1"
 PORT="$2"
 
-echo "Waiting for $HOST:$PORT..."
+echo "Waiting for $HOST:$PORT to be available..."
 
 for i in `seq $TIMEOUT` ; do
     nc -z "$HOST" "$PORT" > /dev/null 2>&1
     result=$?
     if [ $result -eq 0 ] ; then
+        echo "Connection to $HOST:$PORT succeeded."
         exit 0
     fi
+    echo "Attempt $i: Connection to $HOST:$PORT failed."
     sleep 1
 done
 
-echo "Operation timed out" >&2
+echo "Operation timed out after $TIMEOUT seconds" >&2
 exit 1
