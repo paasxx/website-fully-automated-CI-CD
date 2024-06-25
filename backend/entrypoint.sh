cfa2909b483f6c7952d92f1cc931ac1f332fa234
Pedro <paasxx@gmail.com>
2024-06-25 00:25:10 -0300

    Teste

---
+++ backend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Espera o banco de dados estar pronto
+/usr/local/bin/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up"
+
+# Executa as migrações
+python manage.py makemigrations
+python manage.py migrate
+
+# Inicia o servidor Django
+exec python manage.py runserver 0.0.0.0:8000
