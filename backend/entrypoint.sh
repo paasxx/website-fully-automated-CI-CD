f1a8832cb3887fdf1187aa5dd8c0415b19b06595
Pedro <paasxx@gmail.com>
2024-06-26 00:11:17 -0300

    Teste

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,11 @@
 # Espera o banco de dados estar pronto
 # /usr/local/bin/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up"
 
+
+# Adiciona um atraso de 10 segundos
+sleep 20
+
+
 # Executa as migrações
 python manage.py makemigrations
 python manage.py migrate
