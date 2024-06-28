9ce9bad760da68142e14ae08139e660af69fddb7
Pedro <paasxx@gmail.com>
2024-06-28 19:47:05 -0300

    Teste

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-# Espera o banco de dados estar pronto
+# # Espera o banco de dados estar pronto
 # /usr/local/bin/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up"
-
 
 # Adiciona um atraso de 10 segundos
 sleep 20
-
 
 # Executa as migrações
 python manage.py makemigrations
 python manage.py migrate
 
-# # Inicia o servidor Django
-# exec python manage.py runserver 0.0.0.0:8000
-
 # Inicia o servidor Django com Gunicorn
-exec gunicorn kanastra.wsgi:application --bind 0.0.0.0:8000 --workers 3
+gunicorn kanastra.wsgi:application --bind unix:/tmp/gunicorn.sock --workers 3 &
+
+# Inicia o servidor Nginx
+nginx -g "daemon off;"
