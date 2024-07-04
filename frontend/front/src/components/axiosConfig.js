8688820cb6999ac864a7bee426b4d85f6fb1abee
Pedro <paasxx@gmail.com>
2024-07-03 23:38:48 -0300

    Teste

--- frontend/front/src/components/axiosConfig.js
+++ frontend/front/src/components/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL, // Use the environment variable
-    timeout: 100000,  // Timeout em milissegundos (por exemplo, 10 segundos)
+    timeout: 10000,  // Timeout em milissegundos (por exemplo, 10 segundos)
 });
 
 export default axiosInstance;
