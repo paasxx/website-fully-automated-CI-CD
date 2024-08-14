0c54647527e3042779b151080d120c86e42cde99
Pedro <paasxx@gmail.com>
2024-08-13 21:56:47 -0300

    Fix nginx config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -12,6 +12,9 @@ python manage.py migrate
 
 python manage.py collectstatic --no-input
 
+# Cria um superusuário não interativo
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
+
 # Inicia o servidor Django com Gunicorn
 gunicorn kanastra.wsgi:application --bind unix:/tmp/gunicorn.sock --workers 3 &
 
