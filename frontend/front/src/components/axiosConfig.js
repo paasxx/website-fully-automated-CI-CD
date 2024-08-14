68b63b91f58fc9f622e666695873ceca04a1ef54
Pedro <paasxx@gmail.com>
2024-08-13 23:11:45 -0300

    Fix nginx config

--- frontend/front/src/components/axiosConfig.js
+++ frontend/front/src/components/axiosConfig.js
@@ -2,8 +2,33 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL, // Use the environment variable
-    timeout: 100000,  // Timeout em milissegundos (por exemplo, 10 segundos)
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    timeout: 100000,
+});
+
+// Função para pegar o token CSRF do cookie
+function getCSRFToken() {
+    const name = 'csrftoken';
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
+// Interceptor para incluir o token CSRF em cada requisição
+axiosInstance.interceptors.request.use((config) => {
+    const csrfToken = getCSRFToken();
+    if (csrfToken) {
+        config.headers['X-CSRFToken'] = csrfToken;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default axiosInstance;
