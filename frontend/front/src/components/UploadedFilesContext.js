0110d4ca652e4cda5981b3e9d14c67ed3e35670c
Pedro <paasxx@gmail.com>
2024-08-20 11:55:48 -0300

    creating a load balancer

--- frontend/front/src/components/UploadedFilesContext.js
+++ frontend/front/src/components/UploadedFilesContext.js
@@ -19,6 +19,7 @@ export const UploadedFilesProvider = ({ children }) => {
             console.log('API Response:', response.data); // Log the API response
             setUploadedFiles(response.data);
         } catch (error) {
+            console.log('Endpoint:', axiosInstance.get('/list-files/')); // Log the API response
             console.error('Error fetching uploaded files:', error);
         }
     };
