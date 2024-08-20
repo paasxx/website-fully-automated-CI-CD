d6cff9409eb880917ef44d71f8123b23b5408a07
Pedro <paasxx@gmail.com>
2024-08-20 20:26:32 -0300

    creating a load balancer

--- backend/wait-for-it.sh
+++ backend/wait-for-it.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-# Use this script to test if a given TCP host/port are available
+# Use this script to test if a given TCP host/port are available using telnet
 
 TIMEOUT=60
-QUIET=0
 HOST="$1"
 PORT="$2"
+
+if [ -z "$HOST" ] || [ -z "$PORT" ]; then
+    echo "Usage: $0 <host> <port>"
+    exit 1
+fi
 
 echo "Waiting for $HOST:$PORT to be available..."
 
 for i in `seq $TIMEOUT` ; do
     echo "Attempt $i: Checking connection to $HOST:$PORT..."
-    nc -zv "$HOST" "$PORT" > /dev/null 2>&1
+    (echo > /dev/tcp/$HOST/$PORT) >/dev/null 2>&1
     result=$?
     if [ $result -eq 0 ] ; then
         echo "Connection to $HOST:$PORT succeeded."
