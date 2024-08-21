ee3dc07eb31b8d7bbb2d62e27bb0596b36d19a3a
Pedro <paasxx@gmail.com>
2024-08-21 10:00:09 -0300

    creating a load balancer

--- frontend/front/src/components/axiosConfig.js
+++ frontend/front/src/components/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL,
-    timeout: 100000,
+    timeout: 150000,
 });
 
 // Função para pegar o token CSRF do cookie
