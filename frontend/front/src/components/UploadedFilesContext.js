8379796c7a423f2122c0d380d4d68633e562102b
Pedro <paasxx@gmail.com>
2024-08-22 16:19:02 -0300

    ADD health conditions to containers

--- frontend/front/src/components/UploadedFilesContext.js
+++ frontend/front/src/components/UploadedFilesContext.js
@@ -19,7 +19,7 @@ export const UploadedFilesProvider = ({ children }) => {
             console.log('API Response:', response.data); // Log the API response
             setUploadedFiles(response.data);
         } catch (error) {
-            console.error('Error fetching uploaded files:', error);
+            //console.error('Error fetching uploaded files:', error);
         }
     };
 
