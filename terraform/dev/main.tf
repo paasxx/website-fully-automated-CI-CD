92521cea8197e07fecdd42c125589ca8445627a8
Pedro <paasxx@gmail.com>
2024-09-14 18:18:13 -0300

    teste terraform

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,24 +2,335 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Importar o módulo VPC, que já foi criado
-module "vpc" {
-  source = "./terraform/dev/vpc.tf" # Certifique-se de que o caminho esteja correto
+
+
+resource "aws_ecr_repository" "frontend" {
+  name = "frontend-repo"
 }
 
-# Importar o Security Group, se já tiver sido definido
-module "security_group" {
-  source = "./terraform/dev/security_groups.tf" # Certifique-se de que o caminho esteja correto
+resource "aws_ecr_repository" "backend" {
+  name = "backend-repo"
 }
 
-# Cluster ECS (já foi definido no `ecs.tf`)
-module "ecs" {
-  source = "./terraform/dev/ecs.tf"
+# Adiciona o repositório ECR à política de acesso do ECS
+resource "aws_iam_role_policy_attachment" "ecs_ecr" {
+  policy_arn = aws_iam_policy.ecr_access.arn
+  role       = aws_iam_role.ecs_task_role.name
 }
 
-# Load Balancer (importado do módulo ou definido separadamente)
-module "load_balancer" {
-  source = "./terraform/dev/load_balancer.tf"
+
+resource "aws_ecs_cluster" "dev_cluster" {
+  name = "dev-cluster"
 }
 
-# Outros módulos que já foram definidos para manter a estrutura modular
+resource "aws_ecs_task_definition" "frontend" {
+  family                   = "frontend-task"
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name  = "frontend"
+    image = "${aws_ecr_repository.frontend.repository_url}:latest"
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+      protocol      = "tcp"
+    }]
+    environment = [
+      {
+        name  = "REACT_APP_BACKEND_URL"
+        value = "http://${aws_lb.app_lb.dns_name}/api/"
+      }
+    ]
+  }])
+}
+
+resource "aws_ecs_task_definition" "backend_task" {
+  family                   = "dev-backend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "backend"
+      image     = "${aws_ecr_repository.backend_repo.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+        }
+      ]
+      environment = [
+        {
+          name  = "DB_NAME"
+          value = "kanastra_db"
+        },
+        {
+          name  = "DB_USER"
+          value = "kanastra_user"
+        },
+        {
+          name  = "DB_PASSWORD"
+          value = var.db_password
+        },
+        {
+          name  = "DB_HOST"
+          value = "127.0.0.1"
+        },
+        {
+          name  = "DB_PORT"
+          value = "5432"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/kanastra-dev"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "backend"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "db_task" {
+  family                   = "db-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "db"
+      image     = var.db_image # Exemplo: "postgres:13" ou "mysql:8"
+      essential = true
+      environment = [
+        {
+          name  = "POSTGRES_USER"
+          value = var.db_user
+        },
+        {
+          name  = "POSTGRES_PASSWORD"
+          value = var.db_password
+        },
+        {
+          name  = "POSTGRES_DB"
+          value = var.db_name
+        }
+      ]
+      portMappings = [
+        {
+          containerPort = var.db_port # Exemplo: 5432 para PostgreSQL, 3306 para MySQL
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/db"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "db"
+        }
+      }
+    }
+  ])
+}
+
+
+
+
+resource "aws_ecs_service" "frontend_service" {
+  name            = "frontend-service"
+  cluster         = aws_ecs_cluster.dev_cluster.id
+  task_definition = aws_ecs_task_definition.frontend_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = aws_subnet.dev_subnet[*].id
+    assign_public_ip = true
+    security_groups  = [aws_security_group.dev_sg.id]
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend_target_group.arn
+    container_name   = "frontend"
+    container_port   = 80
+  }
+}
+
+resource "aws_ecs_service" "backend_service" {
+  name            = "backend-service"
+  cluster         = aws_ecs_cluster.dev_cluster.id
+  task_definition = aws_ecs_task_definition.backend_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = aws_subnet.dev_subnet[*].id
+    assign_public_ip = true
+    security_groups  = [aws_security_group.dev_sg.id]
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend_target_group.arn
+    container_name   = "backend"
+    container_port   = 8000
+  }
+}
+
+resource "aws_ecs_service" "db_service" {
+  name            = "db-service"
+  cluster         = aws_ecs_cluster.dev_cluster.id
+  task_definition = aws_ecs_task_definition.db_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = aws_subnet.dev_subnet[*].id
+    assign_public_ip = true
+    security_groups  = [aws_security_group.dev_sg.id]
+  }
+}
+
+
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_access" {
+  name        = "ecr_access_policy"
+  description = "Policy to access ECR repositories"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecr_access.arn
+}
+
+resource "aws_lb" "dev_lb" {
+  name                       = "dev-lb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.dev_sg.id]
+  subnets                    = aws_subnet.dev_subnet[*].id
+  enable_deletion_protection = false
+
+  enable_cross_zone_load_balancing = true
+  enable_http2                     = true
+
+  tags = {
+    Name = "dev-lb"
+  }
+}
+
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "frontend-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = dev_vpc.id
+
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.dev_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
+
+resource "aws_lb_target_group" "backend" {
+  name     = "backend-target-group"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = dev_vpc.id
+}
+
+resource "aws_security_group" "dev_sg" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dev-sg"
+  }
+}
+
+resource "aws_vpc" "dev_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "dev_subnet" {
+  count = 2
+
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "dev-subnet-${count.index}"
+  }
+}
+
+
