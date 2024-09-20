f8e5d89c449434c8d99b37d053755a3bc833fa17
Pedro <paasxx@gmail.com>
2024-09-20 00:57:54 -0300

    teste terraform

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -337,15 +337,6 @@ resource "aws_subnet" "dev_subnet" {
   }
 }
 
-resource "aws_s3_bucket" "meu_bucket_terraform" {
-  bucket = "meu-bucket-terraform"
-  acl    = "private"
-  region = "us-east-1"
-  tags = {
-    Name        = "meu-bucket-terraform"
-    Environment = "dev"
-  }
-}
 
 terraform {
   backend "s3" {
