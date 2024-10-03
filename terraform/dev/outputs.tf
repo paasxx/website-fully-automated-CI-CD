77a02eb6efad9e2e5081be8e69787821c4eb5943
Pedro <paasxx@gmail.com>
2024-10-03 19:16:57 -0300

    implementacao do https

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -7,10 +7,10 @@ output "backend_repo_url" {
   value = aws_ecr_repository.backend.repository_url
 }
 
-output "load_balancer_dns" {
-  value = aws_lb.dev_lb.dns_name
-}
-
 output "frontend_lb_dns" {
   value = aws_lb.frontend_lb.dns_name
+}
+
+output "backend_lb_dns" {
+  value = aws_lb.backend_lb.dns_name
 }
